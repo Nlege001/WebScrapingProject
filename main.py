37cb22dd7e49c43e9fe29a67d090f245692df05a
Naol Legesse <nlege001@plattsburgh.edu>
2021-09-19 08:17:38 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# importing the modules
+import urllib
+import ssl
+from urllib.request import Request, urlopen
+
+import bs4
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = Request("https://www.plattsburgh.edu/directory/", headers= {'User-Agent':'Mozilla/5.0'})
+url_contents = urlopen(url).read()
+
+baseURL ="https://www.plattsburgh.edu/directory/"
+driver = webdriver.Chrome(executable_path= "/Users/naol/Downloads/chromedriver")
+driver.get(baseURL)
+
+soup = bs4.BeautifulSoup(url_contents, "html")
+
+div = soup.findAll("div", {'class':'platts-custom-form-style'})
+#print(div)
+
+elem= driver.find_element_by_xpath("//div[@id='findpeople']")
+print(elem.text)
